f87d36d08212455235b632acf102fb2d55d9aedc
BirukAbate <96596904+BirukAbate@users.noreply.github.com>
2023-08-21 11:37:13 +0200

    first version

--- BANK.cpp
+++ BANK.cpp
@@ -9,16 +9,46 @@
 
 void Bank::addTransaction(double amount, const std::string &date) {
 
+    Transaction transaction;
+    transaction.amount = amount;
+    transaction.date = date;
+    transactions.push_back(transaction);
+
 }
 
 void Bank::displayTransactions() const {
-
+    for (const auto& transaction : transactions) {
+        std::cout << "Amount: " << transaction.amount << " Date: " << transaction.date << std::endl;
+    }
 }
 
 void Bank::loadFromFile(const std::string &filename) {
-
+    std::ifstream inFile(filename);
+    if (inFile.is_open()) {
+        double amount;
+        std::string date;
+        while (inFile >> amount >> date) {
+            Transaction transaction;
+            transaction.amount = amount;
+            transaction.date = date;
+            transactions.push_back(transaction);
+        }
+        inFile.close();
+        std::cout << "Transactions loaded from " << filename << std::endl;
+    } else {
+        std::cerr << "Error opening file for reading." << std::endl;
+    }
 }
 
 void Bank::saveToFile(const std::string &filename) const {
-
+    std::ofstream outFile(filename);
+    if (outFile.is_open()) {
+        for (const auto& transaction : transactions) {
+            outFile << transaction.amount << " " << transaction.date << std::endl;
+        }
+        outFile.close();
+        std::cout << "Transactions saved to " << filename << std::endl;
+    } else {
+        std::cerr << "Error opening file for writing." << std::endl;
+    }
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+#include "Bank.h"
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Bank bank;
+
+    bank.addTransaction(100.0, "2023-08-18");
+    bank.addTransaction(-50.0, "2023-08-19");
+
+    bank.displayTransactions();
+    bank.saveToFile("transaction.txt");
+    bank.loadFromFile("transactions.txt");
+    bank.displayTransactions();
+
     return 0;
 }
+

e51183031475d89765ee628f2c9baab220346a86
BirukAbate <96596904+BirukAbate@users.noreply.github.com>
2023-08-23 09:32:56 +0200

    final version

---
+++ cmake-build-debug/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/birukabate/CLionProjects/FinancialTransactionSystem/googletest/googletest
+# Build directory: /Users/birukabate/CLionProjects/FinancialTransactionSystem/cmake-build-debug/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
